eb500fb71075ec296f4b88400d2792751d68a76c
Your Name <you@example.com>
2024-04-25 08:29:31 -0700

    fixing errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = [""35.153.33.185, ""52.72.14.240]
+env.hosts = ["35.153.33.185", "52.72.14.240"]
 
 
 def do_deploy(archive_path):
